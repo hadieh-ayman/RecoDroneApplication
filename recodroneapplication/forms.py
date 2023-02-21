b7a54a214b632fb18ca30f221920bcd9ba1c1f7b
Hadieh Abdulmajeed <ha1905768@qu.edu.qa>
2023-02-21 21:21:35 +0300

    user authentication

--- recodroneapplication/forms.py
+++ recodroneapplication/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired("Email is required"), Email()], render_kw={"placeholder": "Please enter your email"})
--- recodroneapplication/routes.py
+++ recodroneapplication/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from recodroneapplication import app, bcrypt
 from recodroneapplication.forms import LoginForm
-from recodroneapplication import app
+from recodroneapplication.models import User, Drone
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 @app.route('/')
 @app.route('/home')
@@ -9,17 +12,29 @@ def home():
 
 @app.route('/login', methods={'GET', 'POST'})
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@example.com' and form.password.data == '123456':
-            flash('You have successfully logged in!')
-            return redirect(url_for('dashboard'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, False)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password!')
     return render_template('login.html', title='RecoDrone-Login', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html', title='RecoDrone-Dashboard')
+    drone = Drone.query.filter_by(owner=current_user).first()
+    return render_template('dashboard.html', title='RecoDrone-Dashboard', drone=drone)
+
 
